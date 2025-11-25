324a80b815d96dd056cea83fe0c121f51f573894
nasu <c0a24183e@edu.teu.ac.jp>
2025-11-25 14:40:07 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,9 +10,24 @@ DELTA = {
     pg.K_UP : (0, -5),
     pg.K_DOWN : (0, +5),
     pg.K_LEFT : (-5,0),
-    pg.K_RIGHT : (0,+5)
+    pg.K_RIGHT : (+5,0)
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：判定結果タプル(横方向、縦方向)
+    画面内ならTrue,画面外ならFalse
+    """
+
+    yoko,tate = True,True
+    if rct.left < 0 or WIDTH < rct.right: # 横方向のはみだしチェック
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向のはみだしチェック
+        tate = False
+    return yoko,tate
 
 
 def main():
@@ -54,12 +69,20 @@ def main():
                 sum_mv[1] += mv[1]
         
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 
 if __name__ == "__main__":
